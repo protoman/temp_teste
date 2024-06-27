3d2a7c4bb635759bb17d1eefc1899dc5624bb07f
Iuri Fiedoruk <protoman@upperland.net>
2024-06-27 17:59:48 -0300

    Add methods in the controller for Project and Activity.

--- src/main/java/net/upperland/project01/controller/PracticeController.java
+++ src/main/java/net/upperland/project01/controller/PracticeController.java
@@ -3,8 +3,12 @@ package net.upperland.project01.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.upperland.project01.model.dto.ClientDTO;
+import net.upperland.project01.model.entity.ActivityEntity;
 import net.upperland.project01.model.entity.ClientEntity;
+import net.upperland.project01.model.entity.ProjectEntity;
+import net.upperland.project01.service.ActivityService;
 import net.upperland.project01.service.ClientService;
+import net.upperland.project01.service.ProjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +20,12 @@ import org.springframework.web.bind.annotation.*;
 public class PracticeController {
 
     private final ClientService clientService; // for auto-constructors, needs final
+    private final ProjectService projectService;
+    private final ActivityService activityService;
 
     private static final Logger logger = LoggerFactory.getLogger(PracticeController.class);
+
+    // ============= CLIENT ============= //
 
     @GetMapping("client/{name}")
     public ResponseEntity<?> findClient(@PathVariable String name) {
@@ -45,5 +53,51 @@ public class PracticeController {
     public ResponseEntity<?> findRemoteClient(@PathVariable Integer id) throws Exception {
         return ResponseEntity.ok().body(clientService.findRemoteClientByName(id));
     }
+
+    // ============= PROJECT ============= //
+
+    @GetMapping("project/{name}")
+    public ResponseEntity<?> findProject(@PathVariable String name) throws Exception {
+        return ResponseEntity.ok().body(projectService.findProjectByName(name));
+    }
+
+    @PostMapping("project")
+    public ResponseEntity<ProjectEntity> addProject(@Valid @RequestBody ProjectEntity project) {
+        return ResponseEntity.ok().body(projectService.saveProject(project));
+    }
+
+    @PutMapping("project/{id}")
+    public ResponseEntity<ProjectEntity> updateProject(@PathVariable Integer id, @Valid @RequestBody ProjectEntity project) throws Exception {
+        return ResponseEntity.ok().body(projectService.updateProjectById(id, project));
+    }
+
+    @DeleteMapping("project/{id}")
+    public ResponseEntity<ProjectEntity> deleteProjectById(@PathVariable Integer id) throws Exception {
+        return ResponseEntity.ok().body(projectService.deleteProjectById(id));
+    }
+
+    // ============= ACTIVITY ============= //
+
+    @GetMapping("activity/{name}")
+    public ResponseEntity<?> findActivity(@PathVariable String name) throws Exception {
+        return ResponseEntity.ok().body(activityService.findActivityByName(name));
+    }
+
+    @PostMapping("activity")
+    public ResponseEntity<ActivityEntity> addActivity(@Valid @RequestBody ActivityEntity activity) {
+        return ResponseEntity.ok().body(activityService.saveActivity(activity));
+    }
+
+    @PutMapping("activity/{id}")
+    public ResponseEntity<ActivityEntity> updateActivity(@PathVariable Integer id, @Valid @RequestBody ActivityEntity activity) throws Exception {
+        return ResponseEntity.ok().body(activityService.updateActivityById(id, activity));
+    }
+
+    @DeleteMapping("activity/{id}")
+    public ResponseEntity<ActivityEntity> deleteActivityById(@PathVariable Integer id) throws Exception {
+        return ResponseEntity.ok().body(activityService.deleteActivityById(id));
+    }
+
+
 
 }
